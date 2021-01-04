3af7b75c75adb4ac74773a59b254edf9d67e39ef
Brando Lugo <brando.lugo@gmail.com>
2021-01-03 19:23:43 -0600

    Initial Commit

---
+++ Shared/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
